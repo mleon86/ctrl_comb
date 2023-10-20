56d4f2bb89bc4a9674c7a40b482042e413dfa6a6
mleon86 <marcosmiguel.leon@gmail.com>
2023-10-20 13:03:03 -0300

    Agrega pdbb a requeriments problemas con ctrl_comb test

--- ctrl_comb/tests.py
+++ ctrl_comb/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Mark, Modelo
 
-# Create your tests here.
+class ModeloTests(TestCase):
+    @classmethod
+    def setUpTestData(clase):
+        clase.modelo = Modelo.objects.create(
+            mark = Mark.objects.create(descript = "Toyota"),
+            descript = "Rush"
+        )
+    
+    def test_lista(self):
+        self.assertEqual(self.modelo.descript, "Rush")
+        self.assertEqual(self.modelo.mark.descript, "Toyota")
+
+
+    def test_vista_modal(self):
+        response = self.client.get(reverse("control:modelo_edit_modal", kwargs={'pk':self.modelo.id}))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,"Rush")
+        self.assertTemplateUsed(response,"ctrl_comb/modelo_modal.html")
+    
+
+    def test_vista_modelo(self):
+        response = self.client.get(reverse("control:modelo_list"))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,"Rush")
+        self.assertTemplateUsed(response, "ctrl_comb/modelo.html") 
